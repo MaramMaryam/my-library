9c0f0758d1381e7785de008a089d02bacba5b56c
MaryamDev <m.danehvash2014@gmail.com>
2023-11-12 06:33:57 +0330

    add pagegeoup class or model

--- DataLayer/Models/PageGroup.cs
+++ DataLayer/Models/PageGroup.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,11 @@ namespace DataLayer
 {
     public class PageGroup
     {
+        [Key]
+        public int GroupID { get; set; }
+        [Display(Name = "عنوان گروه")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
+        [MaxLength(150)]
+        public string GroupTitle { get; set; }
     }
 }
