d429ea52219d742d361e4fc6282baa9acb7aa7c1
MaryamDev <m.danehvash2014@gmail.com>
2023-12-01 08:40:10 +0330

    add view model to show tedad groups

---
+++ my-library/Controllers/BooksController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace my_library.Controllers
+{
+    public class BooksController : Controller
+    {
+        // GET: Books
+        public ActionResult ShowGroups()
+        {
+            return PartialView();
+        }
+    }
+}
