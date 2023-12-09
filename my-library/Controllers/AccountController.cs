4570db78ef3b660ba90ed94d52338f85b34c31f0
MaryamDev <m.danehvash2014@gmail.com>
2023-12-09 06:40:26 +0330

    adding login page for admin

---
+++ my-library/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace my_library
+{
+    public class AccountController : Controller
+    {
+        private ILoginRepository loginRepository;
+        MyCmsContext db = new MyCmsContext();
+        public AccountController()
+        {
+            loginRepository = new LoginRepository(db);
+        }
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
