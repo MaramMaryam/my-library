c3217773abfbca2874a007c0ef88a5d1e8887925
MaryamDev <m.danehvash2014@gmail.com>
2023-12-11 06:30:28 +0330

    fini

--- DataLayer/Models/Page.cs
+++ DataLayer/Models/Page.cs
@@ -4,6 +4,8 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+//using System.Web.Mvc;
+//using System.Web.Mvc.AllowHtmlAttribute;
 
 namespace DataLayer
 {
@@ -30,10 +32,11 @@ namespace DataLayer
         [MaxLength(350)]
         [DataType(DataType.MultilineText)]
         public string ShortDescription { get; set; }
+
         [Display(Name = "متن")]
         [Required(ErrorMessage = "لطفا {0} را وارد کنید.")]
         [DataType(DataType.MultilineText)]
-
+        //[AllowHtml()]
         public string Text { get; set; }
         [Display(Name = "بازدید")]
         public int Visit { get; set; }
@@ -59,7 +62,6 @@ namespace DataLayer
 
         [Display(Name = "کلمات کلیدی")]
         public string Tags { get; set; }
-
 
         public virtual PageGroup PageGroup { get; set; }
         public virtual List<PageComment> PageComments { get; set; }
